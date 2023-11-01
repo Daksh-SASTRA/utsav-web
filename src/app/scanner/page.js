f0fc0549c11bb808eb6b1ee4a3c6d3b1c8b1444e
bvvinai <venkata.vinai@gmail.com>
2023-11-01 16:35:28 +0530

    added barcode

---
+++ src/app/scanner/page.js
@@ -0,0 +1,49 @@
+"use client"
+import Head from 'next/head'
+// Importing stylesheets
+import styles from './page.module.css'
+
+// Importing components
+import Nav from '../../components/Nav/Nav'
+import BlobGlow from '@/components/BlobGlow/BlobGlow';
+import { Html5QrcodeScanner } from "html5-qrcode";
+import { useEffect, useState } from 'react';
+// import Footer from '../components/Footer/Footer'
+
+function ScannerPage() {
+
+    const [scanResult, setScanResult] = useState(null);
+
+    useEffect(() => {
+        let scanner = new Html5QrcodeScanner(
+            "reader",
+            {
+                fps: 10,
+                qrbox: { width: 250, height: 250 },
+            });
+
+        scanner.render(onScanSuccess, onScanFailure);
+
+        function onScanSuccess(decodedText, decodedResult) {
+            setScanResult(decodedText);
+        }
+
+        function onScanFailure(error) {
+            // setScanResult(error);
+        }
+    }, []);
+
+    return (
+        <>
+            <main className={styles.main}>
+                {
+                    scanResult == null ?
+                        <div id="reader"></div> :
+                        <div id={styles.resultScreen}>{scanResult}</div>
+                }
+            </main>
+        </>
+    )
+}
+
+export default ScannerPage
